a42cef9fbbaeda37b6d74f10cf6cd3cc8e57bc8b
filka <fil92.92@gmail.com>
2026-01-19 12:12:53 +0200

    Залив проекта на git

---
+++ src/main/java/org/example/constants/Actions.java
@@ -0,0 +1,8 @@
+package org.example.constants;
+
+public class Actions {
+    public static final String LOGIN = "LOGIN";
+    public static final String ACTION = "ACTION";
+    public static final String LOGOUT = "LOGOUT";
+    public static final String REGISTER = "REGISTER";
+}
---
+++ src/main/java/org/example/constants/Endpoints.java
@@ -0,0 +1,7 @@
+package org.example.constants;
+
+public class Endpoints {
+
+    public static final String BASE_URL = "http://localhost:8080/";
+    public static final String COMMON_ENDPOINT = "endpoint";
+}
